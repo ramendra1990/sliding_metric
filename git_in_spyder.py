91e7918e9271e67ba44779536ef497203922902b
unknown <ramendrageo@gmail.com>
2024-11-06 18:04:08 +0530

    added code lines to push my python file to my git repo

---
+++ git_in_spyder.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  4 17:57:05 2024
+
+@author: ramendra
+"""
+
+cd D:\ramendra_EPM103\NGRI\scripts
+!git init
+# !git remote add origin https://github.com/ramendra1990/sliding_metric.git
+!git add faster_sliding_morphology.py
+!git commit -m "compared the skimage window method with for loop in 2D in the code"
+!git push -u origin master
+# !git push -u origin master
+github git@github.com
